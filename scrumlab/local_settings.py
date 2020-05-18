10d4308a701df6a1662fb6628dd8f83ee23d7a4c
Karol <krogalski204@gmail.com>
2020-05-18 22:23:12 +0200

    Add project

---
+++ scrumlab/local_settings.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scrumlab',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+    }
+}
